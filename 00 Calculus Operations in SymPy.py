f6080b315cbc23986edafded72a2ed98469d2dd7
Aditya Verma <adityasinghverma314@gmail.com>
2018-06-12 11:41:12 +0530

    Update 00 Calculus Operations in SymPy.py

--- 00 Calculus Operations in SymPy.py
+++ 00 Calculus Operations in SymPy.py
@@ -1,27 +1,21 @@
 from sympy import *
 
-
 #define sympy symbols for 
 x , t , z , nu = symbols ('x t z nu')
 
 #for pretty printing:
 init_printing ( use_unicode=True)
 
-
 #take a derivative of  [ sin (x) e ^ x ]
 print (" Diffrentiating sin (x) e ^ x ") 
 print ( diff ( sin ( x ) * exp (x), x))
-
 
 #integration:
 print ( "\n Integrating the result:")
 print (  integrate ( exp(x) * sin (x) + exp(x)*cos(x) , x) )
 
-
-
 print("\n Integrating with limits   -oo to + oo ")
 print ( integrate( sin(x**2) , ( x, -oo , oo ) ) )
-
 
 print ("\n Finding the limit as x -> 0 " )
 print ( limit (sin(x)/x, x, 0 ) )
